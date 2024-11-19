cade543f98147f318e0f6bb3a1f2b4cc25bdf0e9
Sefinek <sefinek5916@gmail.com>
2024-11-19 10:24:26 +0100

    1.0.1

--- utils/removeByCriteria.js
+++ utils/removeByCriteria.js
@@ -1,12 +1,13 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const { parse } = require('csv-parse');
 
 const txtFilePath = path.join(__dirname, '../lists/main.txt');
 const csvFilePath = path.join(__dirname, '../lists/details.csv');
 
 const removeFromFile = (filePath, patterns) => {
 	if (!fs.existsSync(filePath)) {
-		console.error(`File not found: ${filePath}`);
+		console.error('File not found:', filePath);
 		return 0;
 	}
 
@@ -21,38 +22,26 @@ const removeFromFile = (filePath, patterns) => {
 	}
 };
 
-const parseCSV = csvContent => {
-	return csvContent.split('\n').map(line => {
-		const values = [];
-		let current = '';
-		let insideQuotes = false;
-		for (let i = 0; i < line.length; i++) {
-			const char = line[i];
-			if (char === '"') {
-				insideQuotes = true;
-				current += char;
-			} else if (char === ',' && !insideQuotes) {
-				values.push(current);
-				current = '';
-			} else {
-				current += char;
-			}
-		}
-		values.push(current);
-		return values;
-	});
+const parseCSV = async () => {
+	const csvData = [];
+	const parser = fs.createReadStream(csvFilePath).pipe(parse({ delimiter: ',' }));
+
+	for await (const record of parser) {
+		csvData.push(record);
+	}
+	return csvData;
 };
 
 const filterByCriteria = (csvData, criteria, columnIndex) => csvData.filter(line => line[columnIndex]?.includes(criteria));
 const removeLinesFromCSV = (csvData, matchingLines) => csvData.filter(line => !matchingLines.some(matchingLine => JSON.stringify(line) === JSON.stringify(matchingLine)));
 
-const removeByCriteria = (criteria, criteriaType) => {
+const removeByCriteria = async (criteria, criteriaType) => {
 	if (!criteria || !criteriaType) return console.error('Criteria and criteriaType are required parameters.');
-	if (!fs.existsSync(csvFilePath)) return console.error(`CSV file not found: ${csvFilePath}`);
+	if (!fs.existsSync(csvFilePath)) return console.error('CSV file not found:', csvFilePath);
 
 	try {
-		const csvContent = fs.readFileSync(csvFilePath, 'utf8');
-		const csvData = parseCSV(csvContent);
+		const csvData = await parseCSV();
+
 		let matchingLines = [];
 		if (criteriaType === 'endpoint') {
 			matchingLines = filterByCriteria(csvData, criteria, 4);
